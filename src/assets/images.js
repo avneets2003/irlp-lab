13d80ee0f34f3ce8cbbecef8217486b230ce0ddd
avneets2003 <avneets2003@gmail.com>
2025-02-01 20:43:49 +0530

    Update images.js

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,5 +1,7 @@
+// Step 1: Store the image in 'src/assets/people' and import the image from './people/<image_name>'
 import bhargavDave from './people/bhargav_dave.jpg';
 
+// Step 2: Map the name to the imported image
 // Make sure the name in imageMap is exactly same as what's displayed on the site (no extra spaces or symbols)
 
 export const imageMap = {
