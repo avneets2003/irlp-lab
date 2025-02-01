ba2b50b2137b668071b80ff1a7cd24a51eb2cc1a
avneets2003 <avneets2003@gmail.com>
2025-02-01 20:36:12 +0530

    Make suggested changes

---
+++ src/assets/images.js
@@ -0,0 +1,7 @@
+import bhargavDave from './people/bhargav_dave.jpg';
+
+// Make sure the name in imageMap is exactly same as what's displayed on the site (no extra spaces or symbols)
+
+export const imageMap = {
+  'Bhargav Dave': bhargavDave,
+};
--- src/pages/people/current.js
+++ src/pages/people/current.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UserCard from '../../components/userCard/index';
 import data from '../../data/people.json';
 import { Container, Row, Col } from 'react-bootstrap';
+import { imageMap } from '../../assets/images';
 
 const CurrentPeoplePage = () => {
   return (
@@ -12,11 +13,14 @@ const CurrentPeoplePage = () => {
           <div key={index}>
             <h2 className="mt-4 mb-4">{category.title}</h2>
             <Row className='mx-3 mx-md-0'>
-              {category.people.map((person, idx) => (
-                <Col key={idx} xs={12} md={4} lg={3} className="mb-4">
-                  <UserCard name={person.name} image={person.image} socials={person.socials} />
-                </Col>
-              ))}
+              {category.people.map((person, idx) => {
+                const image = imageMap[person.name] || "https://via.placeholder.com/150";
+                return (
+                  <Col key={idx} xs={12} md={4} lg={3} className="mb-4">
+                    <UserCard name={person.name} image={image} socials={person.socials} />
+                  </Col>
+                );
+              })}
             </Row>
           </div>
         ))}
